d477c8422dc287d2fa62c713febc7826b8eac6c2
SetroZ <joemosabry@gmail.com>
2024-02-26 18:42:45 +0800

    added getlocation

---
+++ data.ts
@@ -0,0 +1,12 @@
+const x = {
+  result: [
+    {
+      geometry: {
+        coordinates: [115.89613736396142, -32.007253740940804],
+      },
+      zValue: 2,
+      dispBldNames: ['314 (New Technologies)'],
+    },
+  ],
+};
+export type locationResponseType = typeof x;
--- utils/formatData.ts
+++ utils/formatData.ts
@@ -1,10 +1,12 @@
 //example of a string
 // 8:00 am-10:00 am<
 
+import { locationResponseType } from '../data';
+
 const timeHandler = (splitted: string[], n = 0) => {
   return { hour: Number(splitted[0]) + n, minutes: Number(splitted[1]) };
 };
-export default function convertTime(timeString: string) {
+export function convertTime(timeString: string) {
   const times = timeString.replace(' ', '').replace(',', '').split('-');
   const formattedTimes = times.map((time) => {
     if (time.match('am')) {
@@ -18,3 +20,32 @@ export default function convertTime(timeString: string) {
   });
   return { start: formattedTimes[0], end: formattedTimes[1] };
 }
+//212 107
+const splitAt = (index: number, array: string | []) => [
+  array.slice(0, index),
+  array.slice(index),
+];
+
+const getURL = (q: string) =>
+  `https://search.mazemap.com/search/equery/?q=${q}&rows=1&start=0&withpois=true&withbuilding=true&withtype=true&withcampus=true&campusid=296&lng=115.89582570734012&lat=-32.00742307052456&boostbydistance=true`;
+export async function getLocation(location: string) {
+  const splitted = splitAt(3, location.replace(' ', ''));
+  const formatted = splitted[0] + '.' + splitted[1];
+  const res = await fetch(getURL(formatted));
+
+  const datares = (await res.json()) as locationResponseType;
+  const data = datares.result[0];
+  return {
+    floor: data.zValue,
+    coordinates: {
+      long: data.geometry.coordinates[1],
+      lat: data.geometry.coordinates[0],
+    },
+    placeName: data.dispBldNames,
+    room: splitted[1],
+  };
+}
+
+getLocation('212 107').then((res) => {
+  console.log(res);
+});
