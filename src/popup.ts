d8587ba34223f2e3c20944667b9d6c5116f008df
SetroZ <joemosabry@gmail.com>
2024-06-23 23:25:20 +0800

    added state + error handling

--- src/popup.ts
+++ src/popup.ts
@@ -3,24 +3,86 @@
 import './popup.css';
 import { command } from './types';
 
-// approximate semester
+// State
+let loading = false;
+const visible = 'inline-block';
+const hidden = 'none';
+const button = document.getElementById(command.download) as HTMLElement;
+const loader = document.getElementById('loader') as HTMLElement;
+const errorElement = document.getElementById('error') as HTMLElement;
+async function setLoading(isVisible: boolean) {
+  if (!isVisible) {
+    loading = false;
+    button.style.display = visible;
+    loader.style.display = hidden;
+  } else {
+    loading = true;
+    button.style.display = hidden;
+    loader.style.display = visible;
+  }
+}
+
+function initializeEvent() {
+  button.addEventListener('click', async () => {
+    try {
+      if (loading) {
+        return;
+      }
+      setLoading(true);
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      const { result, fileName }: { result: string; fileName: string } =
+        (await chrome.tabs.sendMessage(tab.id as number, {
+          command: command.download,
+          semester: getSelectedSemester(),
+        })) as any;
+
+      const icsFile = new File([result as string], fileName, {
+        type: 'text/calendar',
+      });
+      const url = URL.createObjectURL(icsFile);
+      chrome.downloads.download({
+        url: url,
+        filename: fileName,
+      });
+      setLoading(false);
+    } catch (error) {
+      errorElement.style.display = visible;
+      loader.style.display = hidden;
+      const message = errorElement.innerText;
+      errorElement.innerText = errorElement.innerText + ' \n ' + error;
+      setTimeout(async () => {
+        errorElement.style.display = hidden;
+        errorElement.innerText = message;
+
+        setLoading(false);
+      }, 5000);
+    }
+  });
+}
+
+// approximate semester by month
 const yearMonths = [
   [1, 2, 3, 4, 5],
   [6, 7, 8, 9, 10, 11],
 ];
+// set Radio
 function currentSem() {
   const month = new Date().getMonth() + 1;
-  console.log(month);
   if (yearMonths[0].includes(month)) {
     return 1;
   } else if (yearMonths[1].includes(month)) {
     return 2;
   }
 }
-const semesterId = `semester${currentSem()}`;
-console.log(semesterId);
-document.getElementById(semesterId)?.setAttribute('checked', 'true');
-console.log(document.getElementById(semesterId));
+/** Approxmiates current semester and modifies radio button */
+function setDefaultSemester() {
+  const semesterId = `semester${currentSem()}`;
+  document.getElementById(semesterId)?.setAttribute('checked', 'true');
+}
 
 function getSelectedSemester(): 1 | 2 {
   const sem1 = document.getElementById('semester1')?.getAttribute('checked');
@@ -32,24 +94,8 @@ function getSelectedSemester(): 1 | 2 {
   }
 }
 
-document
-  .getElementById(command.download)
-  ?.addEventListener('click', async () => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-    const { result, fileName }: { result: string; fileName: string } =
-      (await chrome.tabs.sendMessage(tab.id as number, {
-        command: command.download,
-        semester: getSelectedSemester(),
-      })) as any;
-    const icsFile = new File([result as string], fileName, {
-      type: 'text/calendar',
-    });
-    const url = URL.createObjectURL(icsFile);
-    chrome.downloads.download({
-      url: url,
-      filename: fileName,
-    });
-  });
+function main() {
+  setDefaultSemester();
+  initializeEvent();
+}
+main();
