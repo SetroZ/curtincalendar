e25850220ccd3d752abf64b0c92c56330434fda6
SetroZ <joemosabry@gmail.com>
2025-02-05 22:10:55 +0800

    removed background from jsons

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -12,7 +12,6 @@ const config = (env, argv) =>
       popup: PATHS.src + '/popup.ts',
       contentScript: PATHS.src + '/contentScript.ts',
       readTable: PATHS.src + '/readTable.ts',
-      background: PATHS.src+ '/background.ts'
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
   });
